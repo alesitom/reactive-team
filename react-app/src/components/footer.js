a8a4dd34552f4ac2b3852b90e6dbce376268adc4
AuFede <afopostingbox@gmail.com>
2021-09-17 20:13:14 -0300

    packaje.json

---
+++ react-app/src/components/footer.js
@@ -0,0 +1,84 @@
+export default "react-app\src\components";
+
+const Footer = () => {
+    return (
+    
+        <div className = "main-footer">
+            <div className = "container-socialnetwork">
+
+            </div>
+
+            <div className = "row">
+
+            {/*Logo*/}
+            <div className = "logo">logo.svg</div>
+            </div>
+
+            <div className = "row"> 
+
+            {/*Occasions*/}
+            <div className = "col">
+                <h6>Occasions</h6>
+                <ul className = "list-one">
+                    <li>Occasions</li>
+                    <li>Working from Home</li>
+                    <li>Snack Time</li>
+                    <li>Breakfast</li>
+                    <li>Vegan Life</li>
+                    <li>Baking</li>
+                    <li>Gifts&Bundles</li>
+                    <li>Easy Cooking</li>
+                    <li>All Products</li>
+                </ul>
+            </div>
+
+            {/*All categories*/}
+            <div className = "col">
+            <h6>All categories</h6>
+            <ul>
+                <li>Baking</li>
+                <li>Breakfast</li>
+                <li>Condiments</li>
+                <li>Drinks</li>
+                <li>Heat Things Up</li>
+                <li>Meal solutions</li>
+                <li>Pantry Staples</li>
+                <li>Simmer Sauces</li>
+                <li>Snacks&Treats</li>
+                <li>Spice Up Your Cooking</li>
+                <li>Sweet Tooth</li>
+                <li>Wine&Cheese</li>
+                <li>All Products</li>
+            </ul>
+            </div>
+
+            {/*Gifts & Bundles*/}
+            <div className = "col">
+            <h6>Gift&Bundles</h6>
+            <ul>
+                <li>Sweet Tooth Special</li>
+                <li>Discover Local</li>
+                <li>Baking Box</li>
+                <li>Spicy Heat Lover's Box</li>
+                <li>Snacking Sensations</li>
+                </ul>
+            </div>
+
+            {/*Our Brands*/}
+            <div className = "col">
+
+            </div>
+
+            {/*About Us*/}
+            <div className = "col">
+
+            </div>
+
+            
+
+            </div>
+        </div>
+    )
+    
+}
+export default Footer;
