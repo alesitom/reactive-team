b291f6bf76eee46a1f32f264f1669595e264d6b8
alesitom <alemendoza1990@gmail.com>
2021-09-21 20:23:07 -0300

    se agrega componente latestOffers y se lo importa en index.js

---
+++ react-app/src/components/latestOffers.js
@@ -0,0 +1,15 @@
+import '../styles/cover.css';
+
+function Cover() {
+  return (
+    <div className="container-cover">
+      <h2>Support Your Local Artisans</h2>
+      <p>Discover brands and their stories</p>
+      <button>
+        Browse brands
+      </button> 
+    </div>
+  );
+}
+
+export default Cover;
