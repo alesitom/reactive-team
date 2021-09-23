0c3aba100bdd8a483ff42d26dbe57f71e208b410
AuFede <afopostingbox@gmail.com>
2021-09-22 22:46:43 -0300

    simulacro de footer

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Footer from './components/footer.js';
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <h1>contenido</h1>
+    <Footer/>
   </React.StrictMode>,
   document.getElementById('root')
 );
