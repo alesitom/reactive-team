c6a4c73128ff903fde7d61d3724a231f24cc2fe9
alesitom <alemendoza1990@gmail.com>
2021-09-24 08:58:41 -0300

    Se mejora lista de whoMakes

--- react-app/src/components/whoMakes.js
+++ react-app/src/components/whoMakes.js
@@ -9,13 +9,12 @@ function WhoMakes() {
       <h3 className="who-makes-section-title">Who Makes <span>Your Food?</span></h3>
       <p className="who-makes-section-subtitle">Our mission is to help grow the local food ecosystem in BC and across Canada. Why?</p>
       <div className="who-makes-items-box">
-        <ul>
-            <li className="who-makes-items"><img src={images.img12}/>
-                Because it connects us to the land we live on, and to the people who live here.</li>
-            <li className="who-makes-items">Because it’s more meaningful to cook and eat something made by a person nearby than by some distant corporation.</li>
-            <li className="who-makes-items">Because good local food makes our corner of the world a better place to live.</li>
-            <li className="who-makes-items">Because it supports local jobs.</li>
-            <li className="who-makes-items">Because it makes more sense for the environment.</li>
+        <ul className="who-makes-items">
+            <li className="who-makes-item-list">Because it connects us to the land we live on, and to the people who live here.</li>
+            <li className="who-makes-item-list">Because it’s more meaningful to cook and eat something made by a person nearby than by some distant corporation.</li>
+            <li className="who-makes-item-list">Because good local food makes our corner of the world a better place to live.</li>
+            <li className="who-makes-item-list">Because it supports local jobs.</li>
+            <li className="who-makes-item-list">Because it makes more sense for the environment.</li>
         </ul>
 
 
