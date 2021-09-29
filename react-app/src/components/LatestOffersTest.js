62e060c0dcfee748f9204c51a8baafad814f440a
alesitom <alemendoza1990@gmail.com>
2021-09-28 23:31:59 -0300

    Refactor en componente latestOffers.js para consumir database via axios. Se mueven archivos de imagenes a public para poder llamarlos desde json. Se agrega dependencias en package.json

--- react-app/src/components/LatestOffersTest.js
+++ react-app/src/components/LatestOffersTest.js
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 
-const LatestOffers = () => {
+const LatestOffersTest = () => {
     const [offerList,setList] = useState({
         offers : [ /* este array viene del database.json */ ]
 })
@@ -39,4 +39,26 @@ return (
   </div>
 );
 }
-export default LatestOffers
+export default LatestOffersTest
+
+
+
+
+
+  
+  /* [
+    {key: 1, offerTitle: "Pepperoni Pizza", offerPrice: "$19.99", realOfferPrice: "$24.99", offerImage:[images.img1]},
+    {key: 2, offerTitle: "Baked Pancakes", offerPrice: "$35.99", realOfferPrice: "$45.99", offerImage:[images.img2]},
+    {key: 3, offerTitle: "Avocado", offerPrice: "$17.99", realOfferPrice: "$20.99", offerImage:[images.img3]},
+    {key: 4, offerTitle: "Bunch of Orange", offerPrice: "$24.99", realOfferPrice: "$29.99", offerImage:[images.img4]}
+  ]; */
+/* const offersMap = offersList.map(offer => (
+  <div className="offer-box-1" key={offer.key}>
+  <img src={offer.offerImage} alt="img1"/>
+  <h4 className="offer-title">{offer.offerTitle}</h4>
+  <p className="offer-price">{offer.offerPrice} <span className="real-offer-price">{offer.realOfferPrice}</span>
+  </p>
+  <button className="quick-shop-button" onClick={() => alert('esto deberia ser un modal, quack!')}>Quick Shop</button>
+  </div>
+)
+); */
