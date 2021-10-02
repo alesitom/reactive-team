bbd2a871fa640f7c7679af05a3d3a2bec0dd34bb
BraianMG <braian.gonzales77@gmail.com>
2021-10-02 15:59:30 -0300

    starting with the modal

--- react-app/src/components/cover.js
+++ react-app/src/components/cover.js
@@ -1,11 +1,16 @@
 import '../styles/cover.css';
+// import swal from 'sweetalert';
+import showAlert from './modal.js'
+
 
 function Cover() {
+  
+
   return (
     <div className="container-cover">
       <h2>Support Your Local Artisans</h2>
       <p>Discover brands and their stories</p>
-      <button>
+      <button className="button">
         Browse brands
       </button> 
     </div>
--- react-app/src/components/userOptions.js
+++ react-app/src/components/userOptions.js
@@ -3,13 +3,20 @@ import '../styles/userOptions.css'
 import search from '../resources/search.svg'
 import profile from '../resources/profile.svg'
 import cart from '../resources/cart-sin-numero.svg'
+import circle from '../resources/circulo.svg'
 
 function UserOptions() {
     return (
         <div className="user-options">
             <img src={search} alt="Search" className="menu-search"/>
             <img src={profile} alt="Profile" className="menu-profile"/>
-            <img src={cart} alt="Cart" className="menu-cart"/>
+            <div className="cart">
+                <img src={cart} alt="Cart" className="menu-cart"/>
+                <div className="circle">
+                    <img src={circle} alt="Quantity of products in the cart" className="quantity-circle"/>
+                    <p className="quantity-products">3</p>
+                </div>
+            </div>
         </div>
     )
 }
